81ee140e32e1c5f3c7833037c744d25ff26d7041
Peter Schmiedeskamp <peter@thoughtspot.net>
2017-11-16 14:05:14 -0800

    using match-selector allows for working traversal

--- socratopher.scm
+++ socratopher.scm
@@ -1,31 +1,33 @@
 ;;; Socratopher
-(require-extension phricken)
-
+(require-library phricken)
+(import (prefix phricken ph:))
 
 ;; Handlers
 
 ;; Site root
 (define (handle-root req)
-  (send-entries
+  (ph:send-entries
    `((i "You are home")
      (i "----------------------------------")
      (i)
-     (1 "This is a link" "/foo")))
-  (send-lastline))
+     (1 "Foo information" "/foo")))
+  (ph:send-lastline))
 
 (define (handle-foo req)
-  (send-entries
+  (ph:send-entries
    `((i "This is a thing that says foo")
      (i "----------------------------------")
      (i)
-     (1 "back home" "/home"))))
+     (1 "back home" "")))
+  (ph:send-lastline))
 
 (define handlers
-  `(,(match-resource "" handle-root)
-    ,(match-resource "/foo" handle-foo)))
+  `(,(ph:match-selector "" handle-root)
+    ,(ph:match-selector "/foo" handle-foo)))
 
-;; Server configuration
-(set! port (make-parameter 7070))
-(set! handlers (make-parameter handlers))
 
-(start-server! #f)
+;; Start the server with appropriate parameters
+(parameterize
+    ((ph:port 7070)
+     (ph:handlers handlers))
+  (ph:start-server! #f))
